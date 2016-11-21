6d55c98b1cdabfa1d0c4654d940130bc06bbaa45
Алина <sh-alinka@mail.ru>
2016-11-21 13:37:28 +1000

    Добавлен новый функционал: счетчик правильных ответов, счетчик времени выполнения

--- question/Question.java
+++ question/Question.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 
 
 public class Question {//класс вопросов
-    String question;//текст вопроса
+    private String question;//текст вопроса
     public Option[] option;//массив вариантов ответа
 
+    public Question(String question, Option [] option){//конструктор
+        this.question = question;
+        this.option = option;
+    }
+    public String getQuestion() {
+        return question;
+    }
 
  }
 
--- question/Test.java
+++ question/Test.java
@@ -1,51 +1,73 @@
 package question;
 
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Random;
 
 public class Test {
-    public static void main (String args[]) {
-        Question q1 = new Question();
-        q1.question = "2+2=";
+    public static void main(String args[]) {
+        Question[] qmass;
+        qmass = new Question[5];                        //заполнение массива вопросов
+        qmass[1] = new Question("2+2= ", new Option[4]);
+        qmass[2] = new Question("8*8=", new Option[4]);
 
-        q1.option = new Option[4];//Массив вариантов ответа
-        q1.option[0] = new Option("", false);                       // первый вопрос и варианты ответа
-        q1.option[1] = new Option("3", false);
-        q1.option[2] = new Option("4", true);
-        q1.option[3] = new Option("5", false);
-
-
-        Question q2 = new Question();
-        q2.question = "8*8=";
-
-        q2.option = new Option[4];//Массив вариантов ответа
-        q2.option[0] = new Option("", false);                       //второй вопрос и варианты ответа
-        q2.option[1] = new Option("56", false);
-        q2.option[2] = new Option("32", false);
-        q2.option[3] = new Option("64", true);
+        qmass[1].option[0] = new Option("", false);    // варианты ответа на первый вопрос
+        qmass[1].option[1] = new Option("3", false);
+        qmass[1].option[2] = new Option("4", true);
+        qmass[1].option[3] = new Option("5", false);
 
 
-        System.out.println("Вопрос 1: " );
-        calculate(q1);
-        System.out.println("\nВопрос 2: " );
-        calculate(q2);
+        qmass[2].option[0] = new Option("", false);      //варианты ответа на второй вопрос
+        qmass[2].option[1] = new Option("56", false);
+        qmass[2].option[2] = new Option("32", false);
+        qmass[2].option[3] = new Option("64", true);
 
+
+        Calendar c = Calendar.getInstance();
+        c.getTime(); //Время начала теста
+
+        int s=0;
+        for (int i = 1; i < 3; i++) {
+            System.out.println("Вопрос " + i + ":");
+
+            Random random = new Random();
+            s= s+calculate(qmass[random.nextInt(2)+1]);  //счетчик правильных ответов, номер вопроса выбирается случайным образом
+
+        }
+
+        Calendar c1 = Calendar.getInstance();
+        c1.getTime();//Время завершения теста
+
+
+        long cDiff = c1.getTimeInMillis()-c.getTimeInMillis();
+        System.out.println("Потрачено времени - " + (cDiff/(1000*60))+ "мин " + ((cDiff/1000)%60)+ "c");
+
+      System.out.println("Количество правильных ответов - "+s);
     }
+
 
     //методика расчета правильности ответа
-    private static void calculate(Question a) {
-            System.out.println(a.question);//вывод текста вопроса
+    public static int calculate(Question a) {
+        System.out.println(a.getQuestion());//вывод текста вопроса
 
-            for (int i = 1; i < a.option.length; i++) {//вывод вариантов ответа
-                System.out.println(i + ")" + a.option[i].getAnswer());
-            }
-
-            Scanner sc = new Scanner(System.in);
-            int p = sc.nextInt();//ответ пользователя
-
-            if (a.option[p].getCorrect() == true)// проверка правильности, введенного ответа
-                System.out.println("Верно");
-            else
-                System.out.println("Не верно");
+        for (int i = 1; i < a.option.length; i++) {//вывод вариантов ответа
+            System.out.println(i + ")" + a.option[i].getAnswer());
         }
+
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();//ответ пользователя
+
+        int m = 0;
+
+        // проверка правильности, введенного ответа
+        if (a.option[p].getCorrect() == true) {
+            m++;
+            return m;
+        }
+
+        return m;
     }
+}
+
 
